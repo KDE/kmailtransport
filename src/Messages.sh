fcc328310d1f95140d3fd567dc023ad4b5c2feee
Montel Laurent <montel@kde.org>
2017-03-09 07:19:54 +0100

    Exclude *tests/

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui -o -iname \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp` -o $podir/libmailtransport5.pot
+$XGETTEXT `find . -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmailtransport5.pot
