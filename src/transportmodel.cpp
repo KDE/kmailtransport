f80a4554f4032c55ccfd22c28ffee1adbf1ba15a
Laurent Montel <montel@kde.org>
2024-04-09 19:32:17 +0200

    Improve model

---
+++ src/transportmodel.cpp
@@ -0,0 +1,96 @@
+/*
+    SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "transportmodel.h"
+#include "transportmanager.h"
+#include <KLocalizedString>
+
+using namespace MailTransport;
+TransportModel::TransportModel(QObject *parent)
+    : QAbstractListModel{parent}
+    , mTransportManager(TransportManager::self())
+{
+    updateComboboxList();
+    connect(mTransportManager, &TransportManager::transportsChanged, this, &TransportModel::updateComboboxList);
+}
+
+TransportModel::~TransportModel() = default;
+
+QVariant TransportModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        return {};
+    }
+#if 0
+    const auto &identity = mIdentityManager->modifyIdentityForUoid(mTransportIds[index.row()]);
+    if (role == Qt::ToolTipRole) {
+        return identity.primaryEmailAddress();
+    }
+    if (role == Qt::FontRole) {
+        if (static_cast<IdentityRoles>(index.column()) == IdentityNameRole) {
+            if (identity.isDefault()) {
+                QFont f;
+                f.setBold(true);
+                return f;
+            }
+        }
+    }
+    if (role != Qt::DisplayRole) {
+        return {};
+    }
+    switch (static_cast<IdentityRoles>(index.column())) {
+    case FullEmailRole:
+        return identity.fullEmailAddr();
+    case EmailRole:
+        return identity.primaryEmailAddress();
+    case UoidRole:
+        return identity.uoid();
+    case IdentityNameRole:
+        return generateIdentityName(identity);
+    case DefaultRole:
+        return identity.isDefault();
+    }
+#endif
+
+    return {};
+}
+
+int TransportModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return mTransportIds.count();
+}
+int TransportModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    constexpr int nbCol = static_cast<int>(TransportRoles::LastColumn) + 1;
+    return nbCol;
+}
+
+QVariant TransportModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (static_cast<TransportRoles>(section)) {
+        case NameRole:
+            return i18nc("@title:column email transport name", "Name");
+        case TransportNameRole:
+            return i18nc("@title:column email transport type", "Type");
+        case TransportIdentifierRole:
+        case DefaultRole:
+            return {};
+        }
+    }
+    return {};
+}
+
+void TransportModel::updateComboboxList()
+{
+    beginResetModel();
+    mTransportIds = mTransportManager->transportIds();
+    endResetModel();
+}
+
+#include "moc_transportmodel.cpp"
---
+++ src/transportmodel.h
@@ -0,0 +1,39 @@
+/*
+    SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "mailtransport_export.h"
+#include <QAbstractListModel>
+namespace MailTransport
+{
+class TransportManager;
+class MAILTRANSPORT_EXPORT TransportModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    enum TransportRoles {
+        NameRole,
+        TransportNameRole,
+        TransportIdentifierRole,
+        DefaultRole,
+        LastColumn = DefaultRole,
+    };
+
+    explicit TransportModel(QObject *parent = nullptr);
+    ~TransportModel() override;
+
+    [[nodiscard]] QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    [[nodiscard]] int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    [[nodiscard]] int columnCount(const QModelIndex &parent) const override;
+    [[nodiscard]] QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+private:
+    MAILTRANSPORT_NO_EXPORT void updateComboboxList();
+    QList<int> mTransportIds;
+    TransportManager *const mTransportManager;
+};
+}
