9b3c40844a8978e052fc9a4c1219b0c2ecb56b83
Laurent Montel <montel@kde.org>
2024-04-15 21:14:09 +0200

    Minor optimization

--- src/transportsortproxymodel.cpp
+++ src/transportsortproxymodel.cpp
@@ -30,9 +30,11 @@ TransportActivitiesAbstract *TransportSortProxyModel::transportActivitiesAbstrac
 
 void TransportSortProxyModel::setTransportActivitiesAbstract(TransportActivitiesAbstract *newTransportActivitiesAbstract)
 {
-    mTransportActivitiesAbstract = newTransportActivitiesAbstract;
-    connect(mTransportActivitiesAbstract, &TransportActivitiesAbstract::activitiesChanged, this, &TransportSortProxyModel::invalidateFilter);
-    invalidateFilter();
+    if (mTransportActivitiesAbstract != newTransportActivitiesAbstract) {
+        mTransportActivitiesAbstract = newTransportActivitiesAbstract;
+        connect(mTransportActivitiesAbstract, &TransportActivitiesAbstract::activitiesChanged, this, &TransportSortProxyModel::invalidateFilter);
+        invalidateFilter();
+    }
 }
 
 #include "moc_transportsortproxymodel.cpp"
