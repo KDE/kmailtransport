0a96e8126aef3512865313535f41cce8d4d31fcc
Laurent Montel <montel@kde.org>
2024-04-16 07:39:58 +0200

    Add transportlistviewtest

---
+++ src/autotests/transportlistviewtest.cpp
@@ -0,0 +1,28 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "transportlistviewtest.h"
+#include "widgets/transportlistview.h"
+#include <QHeaderView>
+#include <QTest>
+QTEST_MAIN(TransportListViewTest)
+TransportListViewTest::TransportListViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TransportListViewTest::shouldHaveDefaultValues()
+{
+    MailTransport::TransportListView w;
+    QVERIFY(w.alternatingRowColors());
+    QCOMPARE(w.selectionMode(), QAbstractItemView::SingleSelection);
+    QCOMPARE(w.selectionBehavior(), QAbstractItemView::SelectRows);
+    QVERIFY(!w.rootIsDecorated());
+    QVERIFY(w.isSortingEnabled());
+    QVERIFY(w.allColumnsShowFocus());
+    QCOMPARE(w.contextMenuPolicy(), Qt::CustomContextMenu);
+    QVERIFY(!w.header()->sectionsMovable());
+}
