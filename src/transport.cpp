6af90311e9e21e323b9ad471402e3dc5a7af1b2b
Laurent Montel <montel@kde.org>
2024-09-04 08:00:57 +0200

    Add clang-format validation job

--- src/transport.cpp
+++ src/transport.cpp
@@ -242,7 +242,7 @@ bool Transport::usrSave()
     }
     TransportManager::self()->emitChangesCommitted();
     if (name() != d->oldName) {
-        Q_EMIT TransportManager::self()->transportRenamed(id(), d->oldName, name());
+        Q_EMIT TransportManager::self() -> transportRenamed(id(), d->oldName, name());
         d->oldName = name();
     }
 
